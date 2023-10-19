965ded213c36043028d428e7619bbe8039eef163
Danil-114195722 <ejyou.user@gmail.com>
2023-10-19 18:06:41 +0300

    add human's bet

--- players/human.py
+++ players/human.py
@@ -1,9 +1,21 @@
 from players.player import Player
 
+from outputting.printing import print_invalid_bet
+
 
 class Human(Player):
     def __init__(self):
         super().__init__()
 
-    def bet(self):
-        pass
+    # сделать ставку
+    def bet(self, comp_bet: int) -> int:
+        while True:
+            try:
+                cur_human_bet = int(input('Ваша ставка: '))
+                if not (comp_bet <= cur_human_bet <= self.money):
+                    raise ValueError('int not in range')
+                break
+            except ValueError:
+                print_invalid_bet()
+
+        return cur_human_bet
