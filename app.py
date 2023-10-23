b5ded5206cab94bc52515bdec1468e7339f2f259
Danil-114195722 <ejyou.user@gmail.com>
2023-10-23 15:04:08 +0300

    add help sketch to start of game

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from game import game
+from outputting.printing import print_help
 
 
 if __name__ == '__main__':
-    main_game = game.Game()
+    # print_help()
+    main_game = game.Game(first_stack='human')
     main_game.start()
--- outputting/printing.py
+++ outputting/printing.py
@@ -4,6 +4,12 @@ from cards.card import Card
 from outputting.text_styles import yellow_bold_text, clean_text, blue_bold_text, italic_text, red_italic_text
 
 
+# печать подсказки с правилами игры при запуске проги
+def print_help() -> None:
+    print('ЗДЕСЬ БУДУТ ПРАВИЛА!!!')
+    input('Continue? [type something] ')
+
+
 # печать карт игроков и стола
 def print_table(human_cards: List[Card], table_cards: List[Card], comp_cards: List[Card] = None) -> None:
     # кол-во разделителя
@@ -21,21 +27,25 @@ def print_table(human_cards: List[Card], table_cards: List[Card], comp_cards: Li
         print(f"{red_italic_text}{'$$$ $$$'.center(delimiter_count, ' ')}{clean_text}" + '\n')
 
     # печать карт стола
-    print(f"{blue_bold_text}{'TABLE'.center(delimiter_count, '-')}\033[0m" + '\n')
+    print(f"{blue_bold_text}{'TABLE'.center(delimiter_count, '-')}{clean_text}" + '\n')
     if table_beauty_cards:
         print(f"{italic_text}{' '.join(table_beauty_cards).center(delimiter_count, ' ')}{clean_text}" + '\n')
     else:
         print()
 
     # печать карт человека
-    print(f"{blue_bold_text}{'HUMAN'.center(delimiter_count, '-')}\033[0m" + '\n')
+    print(f"{blue_bold_text}{'HUMAN'.center(delimiter_count, '-')}{clean_text}" + '\n')
     print(f"{italic_text}{' '.join(human_beauty_cards).center(delimiter_count, ' ')}{clean_text}" + '\n')
-    print(f"\033[1m\033[34m{'-' * delimiter_count}\033[0m")
+    print(f"{blue_bold_text}{'-' * delimiter_count}{clean_text}")
 
 
 # печать названия раунда
 def print_round(round_name: str) -> None:
     print('\n\n' + f'{yellow_bold_text}{round_name}{clean_text}' + '\n\n')
+
+
+def print_invalid_bet() -> None:
+    print(f'{red_italic_text}НЕВЕРНАЯ СТАВКА!!!{clean_text}')
 
 
 def print_money():
