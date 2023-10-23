73bfb445d0af05ef1583c15622e9aa3e653c66dd
Danil-114195722 <ejyou.user@gmail.com>
2023-10-23 15:04:32 +0300

    add comments to every combo in dict

--- players/player.py
+++ players/player.py
@@ -7,6 +7,8 @@ from combinations import combo_define
 class Player:
     def __init__(self):
         self.cards = []
+        self.money = 1000
+        self.cur_bet = 0
 
     def add_card(self, new_card: object):
         self.cards.append(new_card)
@@ -31,20 +33,33 @@ class Player:
         #     {'val': 14, 'suit': 'C'},
         # ]
 
+        # все пары, сеты и каре
         all_same_cards = combo_define.pairs(card_list=mix_cards_vocab)
+        # все карты одинаковой масти
         flash_cards = combo_define.flash(card_list=mix_cards_vocab)
+        # все карты одинаковой масти по порядку
         straight_flash_cards = combo_define.straight_flash(flash_list=flash_cards)
 
         combo_dict = {
+            # старшая карта
             0: sorted([calc_card.get_to_calc() for calc_card in self.cards], key=lambda elem: elem['val']),
+            # одна пара
             1: combo_define.one_pair(all_pairs=all_same_cards),
+            # две пары
             2: combo_define.two_pairs(all_pairs=all_same_cards),
+            # сет
             3: combo_define.three_of_a_kind(all_pairs=all_same_cards),
+            # стрит
             4: combo_define.straight(card_list=mix_cards_vocab),
+            # флеш
             5: flash_cards,
+            # фулл хаус
             6: combo_define.full_house(all_pairs=all_same_cards),
+            # каре
             7: combo_define.four_of_a_kind(all_pairs=all_same_cards),
+            # стрит флеш
             8: straight_flash_cards,
+            # роял флеш
             9: combo_define.royal_flash(straight_flash_list=straight_flash_cards),
         }
 
