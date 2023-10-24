e71f41d7b6f69a2951f76b70a79401d229b25735
Danil-114195722 <ejyou.user@gmail.com>
2023-10-24 14:44:14 +0300

    add red bold text style

--- outputting/text_styles.py
+++ outputting/text_styles.py
@@ -1,6 +1,7 @@
 # стили текста
 yellow_bold_text = "\033[1m\033[33m"
 blue_bold_text = "\033[1m\033[34m"
-italic_text = "\033[3m"
+red_bold_text = "\033[1m\033[31m"
 red_italic_text = "\033[3m\033[31m"
+italic_text = "\033[3m"
 clean_text = "\033[0m"
